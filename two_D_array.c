7f466d74b11a6a9c39b294d9ec58763c5e474ca2
Pratham Devangkumar Modesara <prathammodesara@gmail.com>
2026-03-01 12:20:58 +0530

    2D Array with sum of all elements

---
+++ two_D_array.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+    int rows, cols, i, j;
+    int sum = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+
+    printf("Enter elements:\n");
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("\nMatrix is:\n");
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+            sum += arr[i][j];  
+        }
+        printf("\n");
+    }
+
+    printf("\nSum of all elements = %d\n", sum);
+
+    return 0;
+}
